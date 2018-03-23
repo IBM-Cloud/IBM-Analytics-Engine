c0334ca0258ae720998ab31ac0def5b536d80725
dharmesh <dharjain@in.ibm.com>
2018-03-24 00:31:07 +0530

    Re-factored script to accept external meta-store credentials as arguments

--- customization-examples/associate-external-metastore.sh
+++ customization-examples/associate-external-metastore.sh
@@ -2,13 +2,11 @@
 #-----------------------------------------------------------------------
 # Customization script to point an IAE cluster's, Hive meta-store to an 
 # external mysql database. It is recommended to use Compose for MySQL
-# as an external db. JDBC url shall be specified in following format
+# as an external db. This scripts expects following four arguments:
+# <db_user> <db_password> <db_name> <db_conn_url>
+# Connection url shall be specified in the following format
 # jdbc:mysql://<dbHost>:<dbPort>/<dbName>?createDatabaseIfNotExist=true
 #-----------------------------------------------------------------------
-DB_USER_NAME=<yourUser>
-DB_PWD=<yourPassword>
-DB_NAME=<yourDBName>
-DB_CXN_URL=<youDB jdbc url>
 
 # Helper functions
 
@@ -33,7 +31,7 @@ trackProgress ()
 	tempPercent=0
     while [ "$tempPercent" != "100.0" ]
 	do
-        progressResp=`curl -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
+        progressResp=`curl -k -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
 		tempPercent=$(parseJson "$progressResp" '["Requests"]["progress_percent"]')
 		echo "Progress: $tempPercent"
 		sleep 5s
@@ -43,7 +41,7 @@ trackProgress ()
 	if [ "$tempPercent" == "100.0" ]
 	then
 		# Validate that the request is completed
-		progressResp=`curl -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
+		progressResp=`curl -k -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
 		finalStatus=$(parseJson "$progressResp" '["Requests"]["request_status"]')
 		if [ "$finalStatus" == "COMPLETED" ]
         then
@@ -59,6 +57,18 @@ trackProgress ()
 	fi
 }
 
+# Validate input
+if [ $# -ne 4 ]
+then 
+	 echo "Usage: $0 <db_user> <db_password> <db_name> <db_conn_url>"
+else
+	DB_USER_NAME="$1"
+	DB_PWD="$2"
+	DB_NAME="$3"
+	DB_CXN_URL="$4"
+fi
+
+
 # Actual customization starts here
 if [ "x$NODE_TYPE" == "xmanagement-slave2" ]
 then    
@@ -69,14 +79,13 @@ then
     /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST $CLUSTER_NAME hive-site "javax.jdo.option.ConnectionPassword" $DB_PWD
     /var/lib/ambari-server/resources/scripts/configs.sh -u $AMBARI_USER -p $AMBARI_PASSWORD -port $AMBARI_PORT -s set $AMBARI_HOST $CLUSTER_NAME hive-site "ambari.hive.db.schema.name" $DB_NAME
 	
-    echo 'Restart Hive'
-    response=`curl -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By: ambari' --silent -w "%{http_code}" -X POST -d '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/requests` 
-	echo $response
+    echo 'Restart services/components affected by Hive configuration change'
+    response=`curl -k -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By: ambari' --silent -w "%{http_code}" -X POST -d '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/requests` 
 	
     httpResp=${response:(-3)}
     if [[ "$httpResp" != "202" ]]
     then
-		echo 'Error initiating restart for Hive'
+		echo "Error initiating restart for the affected services, API response: $httpResp"
 		exit 1
     else
 		echo "Request accepted. Hive restart in progress...${response::-3}"
