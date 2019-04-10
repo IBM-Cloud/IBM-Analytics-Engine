90e2f84a80da1d66a39a857cb7631fd8778c20a7
mrmadira <mrmadira@in.ibm.com>
2019-04-10 12:11:25 +0530

    Update associate-external-metastore.sh

--- customization-examples/associate-external-metastore.sh
+++ customization-examples/associate-external-metastore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #-----------------------------------------------------------------------
 # Customization script to point an IAE cluster's, Hive meta-store to an 
-# external mysql database. It is recommended to use Compose for MySQL
+# external mysql database. It is recommended to use PostGreSQL
 # as an external db. This scripts expects following four arguments:
-# <db_user> <db_password> <db_name> <db_conn_url>
+# <db_user> <db_password> <db_name> <db_conn_url> <cluster_password>
 # Connection url shall be specified in the following format
 # jdbc:mysql://<dbHost>:<dbPort>/<dbName>
 #-----------------------------------------------------------------------
@@ -31,7 +31,7 @@ trackProgress ()
 	tempPercent=0
     while [ "$tempPercent" != "100.0" ]
 	do
-        progressResp=`curl -k -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
+        progressResp=`curl -k -u $AMBARI_USER:$CLUSTER_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
 		tempPercent=$(parseJson "$progressResp" '["Requests"]["progress_percent"]')
 		echo "Progress: $tempPercent"
 		sleep 5s
@@ -41,7 +41,7 @@ trackProgress ()
 	if [ "$tempPercent" == "100.0" ]
 	then
 		# Validate that the request is completed
-		progressResp=`curl -k -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
+		progressResp=`curl -k -u $AMBARI_USER:$CLUSTER_PASSWORD -H 'X-Requested-By:ambari' -X GET $progressUrl --silent`
 		finalStatus=$(parseJson "$progressResp" '["Requests"]["request_status"]')
 		if [ "$finalStatus" == "COMPLETED" ]
         then
@@ -58,14 +58,15 @@ trackProgress ()
 }
 
 # Validate input
-if [ $# -ne 4 ]
+if [ $# -ne 6 ]
 then 
-	 echo "Usage: $0 <db_user> <db_password> <db_name> <db_conn_url>"
+	 echo "Usage: $0 <db_user> <db_password> <db_name> <db_conn_url> <cluster_user> <cluster_password>"
 else
 	DB_USER_NAME="$1"
 	DB_PWD="$2"
 	DB_NAME="$3"
 	DB_CXN_URL="$4"
+	CLUSTER_PASSWORD="$5"
 fi
 
 
@@ -75,13 +76,17 @@ fi
 if [ "x$NODE_TYPE" == "xmanagement-slave2" ]
 then 
      echo "Updating Ambari properties"
-     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$AMBARI_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "javax.jdo.option.ConnectionURL" -v $DB_CXN_URL
-     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$AMBARI_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "javax.jdo.option.ConnectionUserName" -v $DB_USER_NAME
-     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$AMBARI_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "javax.jdo.option.ConnectionPassword" -v $DB_PWD
-     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$AMBARI_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "ambari.hive.db.schema.name" -v $DB_NAME   
+     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$CLUSTER_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "javax.jdo.option.ConnectionURL" -v $DB_CXN_URL
+     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$CLUSTER_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "javax.jdo.option.ConnectionUserName" -v $DB_USER_NAME
+     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$CLUSTER_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "javax.jdo.option.ConnectionPassword" -v $DB_PWD
+     python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$CLUSTER_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=hive-site -k "ambari.hive.db.schema.name" -v $DB_NAME   
      
     echo 'Restart services/components affected by Hive configuration change'
-    response=`curl -k -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By: ambari' --silent -w "%{http_code}" -X POST -d '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/requests` 
+    response=`curl -k -u $AMBARI_USER:$CLUSTER_PASSWORD -H 'X-Requested-By: ambari' --silent -w "%{http_code}" -X POST -d '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}' https://$AMBARI_HOST:$
+    
+    
+    
+    PORT/api/v1/clusters/$CLUSTER_NAME/requests` 
 	
     httpResp=${response:(-3)}
     if [[ "$httpResp" != "202" ]]
