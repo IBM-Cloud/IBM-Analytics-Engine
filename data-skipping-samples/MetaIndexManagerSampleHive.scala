7c34c325607888799e73353a0f387b9eef87f2d5
guykhazma <Guy.Khazma@ibm.com>
2020-02-13 16:54:06 +0200

    fix for data skipping samples

--- data-skipping-samples/MetaIndexManagerSampleHive.scala
+++ data-skipping-samples/MetaIndexManagerSampleHive.scala
@@ -21,15 +21,10 @@ object MetaIndexManagerSampleHive {
 
     // Setup the environment
     // configure Stocator
-    spark.sparkContext.hadoopConfiguration.set("fs.stocator.scheme.list", "cos")
-    spark.sparkContext.hadoopConfiguration.set("fs.cos.impl", "com.ibm.stocator.fs.ObjectStoreFileSystem")
-    spark.sparkContext.hadoopConfiguration.set("fs.stocator.cos.impl", "com.ibm.stocator.fs.cos.COSAPIClient")
-    spark.sparkContext.hadoopConfiguration.set("fs.stocator.cos.scheme", "cos")
-
     // for more info on how to config credentials see https://github.com/CODAIT/stocator
     // see https://cloud.ibm.com/docs/services/cloud-object-storage?topic=cloud-object-storage-endpoints for the list of endpoints
     spark.sparkContext.hadoopConfiguration.set("fs.cos.service.endpoint" ,"http://s3.us-south.objectstorage.softlayer.net")
-    spark.sparkContext.hadoopConfiguration.set("fs.cos.$serviceName.access.key", "<accessKey>")
+    spark.sparkContext.hadoopConfiguration.set("fs.cos.service.access.key", "<accessKey>")
     spark.sparkContext.hadoopConfiguration.set("fs.cos.service.secret.key","<secretKey>")
 
     // dataset location
