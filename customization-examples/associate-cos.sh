8c03283c69e9eb924193ec1849f26fb69c703d90
mrmadira <mrmadira@in.ibm.com>
2020-02-14 17:05:37 +0530

    Update associate-cos.sh

--- customization-examples/associate-cos.sh
+++ customization-examples/associate-cos.sh
@@ -118,7 +118,7 @@ fi
 
 # Actual customization starts here
 # Note : For existing HDP 2.6.2 clusters, please use configs.sh for cluster customization
-# For new HDP 2.6.2 or 2.6.5 clusters, the following customization using configs.py will work
+# For new HDP 2.6.2 and above  clusters, the following customization using configs.py will work
 if [ "x$NODE_TYPE" == "xmanagement-slave2" ]
 then 
     echo "Updating Ambari properties"
@@ -126,18 +126,6 @@ then
     /usr/bin/python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$AMBARI_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=core-site -k "fs.cos.myprodservice.endpoint" -v $S3_ENDPOINT
     /usr/bin/python /var/lib/ambari-server/resources/scripts/configs.py -s https --user=$AMBARI_USER --password=$AMBARI_PASSWORD --port=$AMBARI_PORT --action=set --host=$AMBARI_HOST --cluster=$CLUSTER_NAME --config-type=core-site -k "fs.cos.myprodservice.secret.key" -v $S3_SECRET_KEY
 
-
-#    echo 'Restart affected services'
-#    response=`curl -k -u $AMBARI_USER:$AMBARI_PASSWORD -H 'X-Requested-By: ambari' --silent -w "%{http_code}" -X POST -d '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}' https://$AMBARI_HOST:$AMBARI_PORT/api/v1/clusters/$CLUSTER_NAME/requests` 
-	
-#    httpResp=${response:(-3)}
-#    if [[ "$httpResp" != "201" ]]
-#    then
-#		echo "Error initiating restart for the affected services, API response: >$httpResp<"
-#		exit 1
-#    else
-#		echo "Request accepted. Service restart in progress...${response::-3}"
-#		trackProgress "${response::-3}"
 
 	stopService HDFS
 	startService HDFS
