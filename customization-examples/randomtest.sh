3ea79342dd18e899625d0dc8500b2b6b4b28208a
Mrudula Madiraju <mrmadira@in.ibm.com>
2021-02-06 21:48:53 +0530

    Update randomtest.sh

--- customization-examples/randomtest.sh
+++ customization-examples/randomtest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #if [ "x$NODE_TYPE" == "management2" ]
 #then 
-  pip3 install xlwt
+  /home/common/conda/miniconda3.7/bin/pip3 install xlwt
   hdfs dfs -mkdir /user/clsadmin/mytest
   hdfs dfs -put /home/common/lib/dataconnectorDb2/db2jcc4.jar /user/clsadmin/mytest/
   #sleep 120
