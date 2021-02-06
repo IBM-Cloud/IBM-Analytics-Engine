5137d7863badf42c570c66e71384075e1a51ecf0
Mrudula Madiraju <mrmadira@in.ibm.com>
2021-02-06 21:46:10 +0530

    Update randomtest.sh

--- customization-examples/randomtest.sh
+++ customization-examples/randomtest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-if [ "x$NODE_TYPE" == "management2" ]
-then 
-  pip install xlwt
+#if [ "x$NODE_TYPE" == "management2" ]
+#then 
+  pip3 install xlwt
   hdfs dfs -mkdir /user/clsadmin/mytest
   hdfs dfs -put /home/common/lib/dataconnectorDb2/db2jcc4.jar /user/clsadmin/mytest/
   #sleep 120
   touch /home/wce/clsadmin/mgmt_node2
- fi
+ #fi
