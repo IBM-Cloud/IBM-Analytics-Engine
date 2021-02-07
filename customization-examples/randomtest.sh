763a200046ca21e58e9838ad495e5465f423c7ad
Mrudula Madiraju <mrmadira@in.ibm.com>
2021-02-07 09:52:30 +0530

    Update randomtest.sh

--- customization-examples/randomtest.sh
+++ customization-examples/randomtest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 echo "$NODE_TYPE <-- NodeType"
 echo "$NODE_TYP <-- New NodeType"
-if [ "x$NODE_TYPE" == "xmanagement-slave1" ]
+if [ "x$NODE_TYPE" == "xmanagement-slave2" ]
 then 
   /home/common/conda/miniconda3.7/bin/pip3 install xlwt
   hdfs dfs -mkdir /user/clsadmin/mytest
