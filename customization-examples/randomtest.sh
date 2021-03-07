bde273771aa40ff22e404dd9f679d498fe1d0157
Mrudula Madiraju <mrmadira@in.ibm.com>
2021-03-07 19:23:39 +0530

    Update randomtest.sh

--- customization-examples/randomtest.sh
+++ customization-examples/randomtest.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 echo "$NODE_TYPE <-- NodeType"
 echo "$NODE_TYP <-- New NodeType"
-if [ "x$NODE_TYPE" == "xmanagement-slave1" ]
-then 
+ 
   /home/common/conda/miniconda3.7/bin/pip3 install xlwt
   hdfs dfs -mkdir /user/clsadmin/mytest
   hdfs dfs -put /home/common/lib/dataconnectorDb2/db2jcc4.jar /user/clsadmin/mytest/
   #sleep 120
   touch /home/wce/clsadmin/mgmt_node2
- fi
