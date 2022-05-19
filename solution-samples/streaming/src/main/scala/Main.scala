cbf7d7fd66a2216f065396a86a4d1d1500c4b53c
Mrudula Madiraju <mrmadira@in.ibm.com>
2022-05-19 09:41:02 +0530

    Update Main.scala

--- solution-samples/streaming/src/main/scala/Main.scala
+++ solution-samples/streaming/src/main/scala/Main.scala
@@ -1,10 +1,11 @@
 package main
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.streaming.Trigger
+import org.apache.spark.sql.SparkSession
 
 object Main {
 
@@ -12,21 +13,14 @@ object Main {
 
     val conf = new SparkConf()
       .setAppName("Structured Streaming from Event Stream to COS")
-
-    val spark = SparkSession
-      .builder()
-      .config(conf)
-      .getOrCreate()
-
+   
+    val spark = SparkSession.builder.getOrCreate()
     import spark.implicits._
-
-    val bucketName = conf.get("spark.s3_bucket")
-
-    // arbitrary name for refering to the cos settings from this code
-    val serviceName = "myservicename"
-
+   
     val sc = spark.sparkContext
-    val s3Url = "cos://${bucketName}." + args(0)
+
+    //exmple: cos://matrix.mycosservice
+    val s3Url = conf.get("spark.cos.url")
 
     //Define the schema of the incoming data 
     val schema = new StructType()
@@ -42,7 +36,7 @@ object Main {
     val df = spark.readStream.
       format("kafka").
       option("kafka.bootstrap.servers", conf.get("spark.kafka_bootstrap_servers")).
-      option("subscribe", "webserver").
+      option("subscribe", conf.get("spark.kafka_topic")).
       option("kafka.security.protocol", "SASL_SSL").
       option("kafka.sasl.mechanism", "PLAIN").
       option("kafka.ssl.protocol", "TLSv1.2").
@@ -50,23 +44,24 @@ object Main {
       option("failOnDataLoss", "false").
       load()
 
+    // Data Filtering/Transformation
     val dataDf = df.selectExpr("CAST(value AS STRING) as json").
       select( from_json($"json", schema=schema).as("data")).
       select("data.*").
       filter($"PROFESSION".isNotNull).   //Filter out the rows where Profession is null 
       filter($"PURCHASE_AMOUNT".isNotNull).  //Filter out the rows where Purchase Amount is null 
       filter($"GENDER" === "M") //Filter out the rows where Gender is Male
-      
+     
 
     val trigger_time_ms = conf.get("spark.trigger_time_ms").toInt
 
     //Write the stream to the COS bucket as a csv file 
     dataDf.
       writeStream.
-      format("parquet"). //This stores the data on COS as a Parquet file. it can be changed to "csv" or "json" if required. 
+      format("parquet"). //This stores the data on COS as a parquet file. it can be changed to "csv" or "json" if required. 
       trigger(Trigger.ProcessingTime(trigger_time_ms)).
-      option("checkpointLocation", s"${s3Url}/checkpoint").
-      option("path", s"${s3Url}/data").
+      option("checkpointLocation", s"${s3Url}/checkpoint2s").
+      option("path", s"${s3Url}/kafkadata").
       option("header", "true"). 
       start()
 
